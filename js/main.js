287721f581330d3c19e1b30ef937ed93fd3d0534
Carol Ω <Carol@iMac-de-Carol-O.local>
2020-05-27 19:31:09 -0300

    terceira aula de introducao ao javascript: condicionais, lacos de repeticao e data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/**var nome = "Caroline Castro";
+/*
+//aula 1
+var nome = "Caroline Castro";
 var idade = 29;
 var idade2 = 10; //se transformados em string o alert as concatena
 var frase = "Japao e o melhor time do mundo"
@@ -13,6 +15,8 @@ console.log(frase.toLowerCase);  //coloca o texto em caixa baixa
 
 //o console pode ser usado para debigar o codigo*/
 
+/*
+//aula 2
 var lista = ["maca", "pera", "laranja"]; //array
 
 //var fruta = {nome:"maca", cor:"vermelha"} //diciionario
@@ -32,4 +36,28 @@ console.log(lista.join("-")); //separa os elementos da lista com o que estiver e
 console.log(fruta.nome);
 alert(fruta.cor);
 
-console.log(frutas);
+console.log(frutas);*/
+
+//aula 3
+var idade = prompt("Qual a sua idade?"); //prompt exibe uma pergunta com uma caixa de texto para o usuario escrever
+//var idade = 18;
+if (idade >= 18) {
+  alert("maior de idade");
+} else {
+  alert("menor de idade");
+}
+
+var count = 5;
+while (count <= 5) {
+  console.log(count);
+  alert(count);
+  count++;
+}
+
+var i;
+for (i = 0; i <= 5; i++) {
+  alert(i);
+}
+
+var d = new Date();
+alert(d.getMonth() + 1);
